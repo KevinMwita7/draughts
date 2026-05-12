d5f9db68b04e7a36e8262f1dd134e6087be62fc5
kevinmwita <kevinmwita7@gmail.com>
2026-05-12 22:58:38 +0400

    Add project files.

---
+++ draughts.cpp
@@ -0,0 +1,12 @@
+﻿// draughts.cpp : Defines the entry point for the application.
+//
+
+#include "draughts.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
