92f6507a19e6cb7e09272f873b86c4b7620ae25a
kevinmwita <kevinmwita7@gmail.com>
2026-06-06 18:21:38 +0400

    Implement Position::do_move

--- src/position.cpp
+++ src/position.cpp
@@ -1,14 +1,48 @@
 #include "draughts/position.h"
 #include "draughts/bitboard.h"
+#include "draughts/zobrist.h"
 
 namespace draughts {
 
-Piece Position::piece_on(Square s) const noexcept {
-    Bitboard mask = sq_bb(s);
-    for (int c = 0; c < 2; ++c)
-        for (int k = 0; k < 2; ++k)
-            if (bb[c][k] & mask) return make_piece(Color(c), PieceKind(k));
-    return NO_PIECE;
-}
+    void Position::do_move(Move m) {
+        const Color   us   = side_to_move;
+        const Color   them = ~us;
+        const PieceKind kind = (bb[us][KING] & sq_bb(m.from)) ? KING : MAN;
+
+        // Update hash before mutating bitboards (captured piece types still readable)
+        hash ^= zobrist::piece_key(m.from, make_piece(us, kind));
+        Bitboard caps = m.captured;
+        while (caps) {
+            Square s = pop_lsb(caps);
+            PieceKind ck = (bb[them][KING] & sq_bb(s)) ? KING : MAN;
+            hash ^= zobrist::piece_key(s, make_piece(them, ck));
+        }
+        hash ^= zobrist::piece_key(m.to, make_piece(us, m.promotion ? KING : kind));
+        hash ^= zobrist::SIDE_KEY;
+
+        // Mutate bitboards
+        bb[us][kind]   ^= sq_bb(m.from) | sq_bb(m.to);
+        bb[them][MAN]  &= ~m.captured;
+        bb[them][KING] &= ~m.captured;
+        if (m.promotion) {
+            bb[us][MAN]  &= ~sq_bb(m.to);
+            bb[us][KING] |=  sq_bb(m.to);
+        }
+
+        occupied = bb[BLACK][MAN] | bb[BLACK][KING] | bb[WHITE][MAN] | bb[WHITE][KING];
+        empty_sq = ~occupied;
+
+        side_to_move = them;
+        ++ply;
+        reversible = m.captured ? 0 : reversible + 1;
+    }
+
+    Piece Position::piece_on(Square s) const noexcept {
+        Bitboard mask = sq_bb(s);
+        for (int c = 0; c < 2; ++c)
+            for (int k = 0; k < 2; ++k)
+                if (bb[c][k] & mask) return make_piece(Color(c), PieceKind(k));
+        return NO_PIECE;
+    }
 
 } // namespace draughts
---
+++ test/position.cpp
@@ -0,0 +1,172 @@
+#include <gtest/gtest.h>
+
+#include "draughts/bitboard.h"
+#include "draughts/position.h"
+#include "draughts/zobrist.h"
+
+using namespace draughts;
+
+class DoMoveTest : public ::testing::Test {
+ protected:
+  void SetUp() override { zobrist::init(); }
+
+  static Position make_pos(Bitboard b_men, Bitboard b_kings, Bitboard w_men,
+                           Bitboard w_kings, Color side, uint16_t ply = 0,
+                           uint8_t reversible = 0) {
+    Position pos{};
+    pos.bb[BLACK][MAN]  = b_men;
+    pos.bb[BLACK][KING] = b_kings;
+    pos.bb[WHITE][MAN]  = w_men;
+    pos.bb[WHITE][KING] = w_kings;
+    pos.occupied        = b_men | b_kings | w_men | w_kings;
+    pos.empty_sq        = ~pos.occupied;
+    pos.side_to_move    = side;
+    pos.hash            = compute_hash(pos);
+    pos.ply             = ply;
+    pos.reversible      = reversible;
+    return pos;
+  }
+};
+
+// ---- piece bitboards --------------------------------------------------------
+
+TEST_F(DoMoveTest, BlackManMovesFromSourceToDestination) {
+  // sq 9 (r2 even) -> sq 13 (r3 odd) via shift_up_right (+4)
+  Position pos = make_pos(sq_bb(9), 0, 0, 0, BLACK);
+  pos.do_move({9, 13, 0, false});
+  EXPECT_EQ(pos.bb[BLACK][MAN], sq_bb(13));
+}
+
+TEST_F(DoMoveTest, WhiteManMovesFromSourceToDestination) {
+  // sq 21 (r5 odd) -> sq 17 (r4 even) via shift_down_left (-4)
+  Position pos = make_pos(0, 0, sq_bb(21), 0, WHITE);
+  pos.do_move({21, 17, 0, false});
+  EXPECT_EQ(pos.bb[WHITE][MAN], sq_bb(17));
+}
+
+TEST_F(DoMoveTest, BlackKingMovesFromSourceToDestination) {
+  // sq 13 (r3 odd) -> sq 17 (r4 even) via shift_up_left (+4)
+  Position pos = make_pos(0, sq_bb(13), 0, 0, BLACK);
+  pos.do_move({13, 17, 0, false});
+  EXPECT_EQ(pos.bb[BLACK][KING], sq_bb(17));
+  EXPECT_EQ(pos.bb[BLACK][MAN], Bitboard{0});
+}
+
+TEST_F(DoMoveTest, QuietMoveDoesNotAffectOpponentBitboards) {
+  Position pos = make_pos(sq_bb(9), 0, sq_bb(21), 0, BLACK);
+  pos.do_move({9, 13, 0, false});
+  EXPECT_EQ(pos.bb[WHITE][MAN], sq_bb(21));
+}
+
+// ---- captures ---------------------------------------------------------------
+
+TEST_F(DoMoveTest, BlackCaptureRemovesOpponentManFromBitboard) {
+  // sq 5 (r1 odd) captures sq 9 (r2 even), lands on sq 12 (r3 odd)
+  Position pos = make_pos(sq_bb(5), 0, sq_bb(9), 0, BLACK);
+  pos.do_move({5, 12, sq_bb(9), false});
+  EXPECT_EQ(pos.bb[BLACK][MAN], sq_bb(12));
+  EXPECT_EQ(pos.bb[WHITE][MAN], Bitboard{0});
+}
+
+TEST_F(DoMoveTest, WhiteCaptureRemovesOpponentManFromBitboard) {
+  // sq 21 (r5 odd) captures sq 17 (r4 even), lands on sq 12 (r3 odd)
+  Position pos = make_pos(sq_bb(17), 0, sq_bb(21), 0, WHITE);
+  pos.do_move({21, 12, sq_bb(17), false});
+  EXPECT_EQ(pos.bb[WHITE][MAN], sq_bb(12));
+  EXPECT_EQ(pos.bb[BLACK][MAN], Bitboard{0});
+}
+
+TEST_F(DoMoveTest, MultipleCapturesRemoveAllOpponentPieces) {
+  // sq 1 (r0 even) captures sq 5 (r1), intermediate sq 10 (r2),
+  // captures sq 14 (r3), lands on sq 19 (r4)
+  Position pos = make_pos(sq_bb(1), 0, sq_bb(5) | sq_bb(14), 0, BLACK);
+  pos.do_move({1, 19, sq_bb(5) | sq_bb(14), false});
+  EXPECT_EQ(pos.bb[BLACK][MAN], sq_bb(19));
+  EXPECT_EQ(pos.bb[WHITE][MAN], Bitboard{0});
+}
+
+// ---- promotion --------------------------------------------------------------
+
+TEST_F(DoMoveTest, BlackManOnPromoRankBecomesKing) {
+  // sq 25 (r6 even) -> sq 29 (r7 BLACK_PROMO_RANK)
+  Position pos = make_pos(sq_bb(25), 0, 0, 0, BLACK);
+  pos.do_move({25, 29, 0, true});
+  EXPECT_EQ(pos.bb[BLACK][MAN],  Bitboard{0});
+  EXPECT_EQ(pos.bb[BLACK][KING], sq_bb(29));
+}
+
+TEST_F(DoMoveTest, WhiteManOnPromoRankBecomesKing) {
+  // sq 4 (r1 odd) -> sq 0 (r0 WHITE_PROMO_RANK)
+  Position pos = make_pos(0, 0, sq_bb(4), 0, WHITE);
+  pos.do_move({4, 0, 0, true});
+  EXPECT_EQ(pos.bb[WHITE][MAN],  Bitboard{0});
+  EXPECT_EQ(pos.bb[WHITE][KING], sq_bb(0));
+}
+
+// ---- occupied ---------------------------------------------------------------
+
+TEST_F(DoMoveTest, OccupiedUpdatedAfterQuietMove) {
+  Position pos = make_pos(sq_bb(9), 0, 0, 0, BLACK);
+  pos.do_move({9, 13, 0, false});
+  EXPECT_EQ(pos.occupied, sq_bb(13));
+}
+
+TEST_F(DoMoveTest, OccupiedClearsCapturedSquares) {
+  Position pos = make_pos(sq_bb(5), 0, sq_bb(9), 0, BLACK);
+  pos.do_move({5, 12, sq_bb(9), false});
+  EXPECT_EQ(pos.occupied, sq_bb(12));
+}
+
+// ---- side to move -----------------------------------------------------------
+
+TEST_F(DoMoveTest, SideToMoveFlipsBlackToWhite) {
+  Position pos = make_pos(sq_bb(9), 0, 0, 0, BLACK);
+  pos.do_move({9, 13, 0, false});
+  EXPECT_EQ(pos.side_to_move, WHITE);
+}
+
+TEST_F(DoMoveTest, SideToMoveFlipsWhiteToBlack) {
+  Position pos = make_pos(0, 0, sq_bb(21), 0, WHITE);
+  pos.do_move({21, 17, 0, false});
+  EXPECT_EQ(pos.side_to_move, BLACK);
+}
+
+// ---- hash -------------------------------------------------------------------
+
+TEST_F(DoMoveTest, HashConsistentAfterQuietMove) {
+  Position pos = make_pos(sq_bb(9), 0, 0, 0, BLACK);
+  pos.do_move({9, 13, 0, false});
+  EXPECT_EQ(pos.hash, compute_hash(pos));
+}
+
+TEST_F(DoMoveTest, HashConsistentAfterCapture) {
+  Position pos = make_pos(sq_bb(5), 0, sq_bb(9), 0, BLACK);
+  pos.do_move({5, 12, sq_bb(9), false});
+  EXPECT_EQ(pos.hash, compute_hash(pos));
+}
+
+TEST_F(DoMoveTest, HashConsistentAfterPromotion) {
+  Position pos = make_pos(sq_bb(25), 0, 0, 0, BLACK);
+  pos.do_move({25, 29, 0, true});
+  EXPECT_EQ(pos.hash, compute_hash(pos));
+}
+
+// ---- counters ---------------------------------------------------------------
+
+TEST_F(DoMoveTest, PlyIncrements) {
+  Position pos = make_pos(sq_bb(9), 0, 0, 0, BLACK, 3);
+  pos.do_move({9, 13, 0, false});
+  EXPECT_EQ(pos.ply, uint16_t{4});
+}
+
+TEST_F(DoMoveTest, ReversibleIncrementsOnQuietMove) {
+  Position pos = make_pos(sq_bb(9), 0, 0, 0, BLACK, 0, 7);
+  pos.do_move({9, 13, 0, false});
+  EXPECT_EQ(pos.reversible, uint8_t{8});
+}
+
+TEST_F(DoMoveTest, ReversibleResetsToZeroOnCapture) {
+  Position pos = make_pos(sq_bb(5), 0, sq_bb(9), 0, BLACK, 0, 5);
+  pos.do_move({5, 12, sq_bb(9), false});
+  EXPECT_EQ(pos.reversible, uint8_t{0});
+}
